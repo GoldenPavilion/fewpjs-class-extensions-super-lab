a5327ed9ced7cd038cb69565eb84f477d15049bb
GoldenPavilion <rcorystine@gmail.com>
2021-08-31 19:12:18 -0400

    classes initialize with parameters using super

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Write your classes here
 
 class Tree {
-
+    constructor(species) {
+        this.species = species;
+    }
 }
 
 class Deciduous extends Tree {
-
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
 }
 
 class Evergreen extends Tree {
-    
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
 }
